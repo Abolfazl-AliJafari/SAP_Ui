bda58d04d7a72454be664f8cb0cd6b7357762783
Abolfazl Alijafari <134207588+Radisix@users.noreply.github.com>
2023-06-19 21:08:22 +0330

    OperationCreate

--- DataAccessLayer/OperationResult.cs
+++ DataAccessLayer/OperationResult.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    internal class Class1
+    public class Class1
     {
+        public class OperationResult
+        {
+            public bool Success { get; set; }
+            public string Message { get; set; }
+        }
+        public class OperationResult<T> : OperationResult
+        {
+            public T Data { get; set; }
+        }
     }
 }
